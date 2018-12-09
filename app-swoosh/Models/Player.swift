0e71a29c7b62cae27725edeec68c0084289ec032
Banji Adewumi <banji@Banjis-Mac.local>
2018-12-08 22:30:37 -0800

    passing data between viewcontrollers

---
+++ app-swoosh/Models/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Banji Adewumi on 12/8/18.
+//  Copyright © 2018 Banji Adewumi. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String!
+    var selectedSkillLevel: String!
+}
